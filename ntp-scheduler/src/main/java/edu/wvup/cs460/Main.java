e6381fef7ddb214509f9e71f155aa992c8a7c2d5
Tom Byrne <kylar42@gmail.com>
2013-02-19 16:11:11 -0500

    handlers

--- ntp-scheduler/src/main/java/edu/wvup/cs460/Main.java
+++ ntp-scheduler/src/main/java/edu/wvup/cs460/Main.java
@@ -1,4 +1,24 @@
 package edu.wvup.cs460;
 
+import edu.wvup.cs460.http.HttpServerPipelineFactory;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 public class Main{
+    public static void main(String[] args) {
+        // Configure the server.
+                  ServerBootstrap bootstrap = new ServerBootstrap(
+                                  new NioServerSocketChannelFactory(
+                                          Executors.newCachedThreadPool(),
+                                          Executors.newCachedThreadPool()));
+
+                  // Set up the event pipeline factory.
+                  bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+
+                  // Bind and start to accept incoming connections.
+                  bootstrap.bind(new InetSocketAddress(8080));
+    }
 }
---
+++ ntp-scheduler/src/main/java/edu/wvup/cs460/http/HttpServerPipelineFactory.java
@@ -0,0 +1,37 @@
+package edu.wvup.cs460.http;
+
+import static org.jboss.netty.channel.Channels.*;
+
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpContentCompressor;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+
+/**
+ * @author <a href="http://www.jboss.org/netty/">The Netty Project</a>
+ * @author Andy Taylor (andy.taylor@jboss.org)
+ * @author <a href="http://gleamynode.net/">Trustin Lee</a>
+ * @version $Rev: 2080 $, $Date: 2010-01-26 18:04:19 +0900 (Tue, 26 Jan 2010) $
+ */
+public class HttpServerPipelineFactory implements ChannelPipelineFactory {
+    public ChannelPipeline getPipeline() throws Exception {
+        // Create a default pipeline implementation.
+        ChannelPipeline pipeline = pipeline();
+
+        // Uncomment the following line if you want HTTPS
+        //SSLEngine engine = SecureChatSslContextFactory.getServerContext().createSSLEngine();
+        //engine.setUseClientMode(false);
+        //pipeline.addLast("ssl", new SslHandler(engine));
+
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        // Uncomment the following line if you don't want to handle HttpChunks.
+        pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+        // Remove the following line if you don't want automatic content compression.
+        pipeline.addLast("deflater", new HttpContentCompressor());
+        pipeline.addLast("handler", new DefaultHttpServerHandler());
+        return pipeline;
+    }
+}
